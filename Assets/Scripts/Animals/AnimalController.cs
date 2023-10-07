f79bad9c152dfdacbd2f8ed13c86118045fdc56d
Lena Musse <lm138@hdm-stuttgart.de>
2023-10-07 11:53:18 +0200

    :sparkles: Add Animal with simple movement

---
+++ Assets/Scripts/Animals/AnimalController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Animals
+{
+    public class AnimalController: MonoBehaviour
+
+    {
+        public MovementController MovementController;
+        
+        private Transform characterTransform;
+
+        private void Start()
+        {
+            characterTransform = transform;
+        }
+
+        private void Update()
+        {
+            MovementController.Move(characterTransform);
+        }
+    }
+}
---
+++ Assets/Scripts/Animals/MovementController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Animals
+{
+    public class MovementController : MonoBehaviour
+    {
+        [SerializeField] private CharacterController characterController;
+        [SerializeField] private float movementSpeed;
+        [SerializeField] private Vector3 movementDirection;
+
+        public void Move(Transform characterTransform)
+        {
+            // Rotate in the right direction
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            characterTransform.rotation = Quaternion.RotateTowards(characterTransform.rotation, toRotation, 700 * Time.deltaTime);
+            
+            // Move 
+            characterController.SimpleMove(movementDirection.normalized * movementSpeed);
+        }
+    }
+}
