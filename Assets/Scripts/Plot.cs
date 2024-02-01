c37832c8a1b41772bcecd30d2d082ef49b2b2355
Lena Musse <lm138@hdm-stuttgart.de>
2024-02-01 17:54:54 +0100

    version

--- Assets/Scripts/Plot.cs
+++ Assets/Scripts/Plot.cs
@@ -18,7 +18,7 @@ namespace DefaultNamespace
         private string plotFilePath = "";
         private string envFilePath = "";
         private string aniFilePath = "";
-        private string buildNumber = "v14-";
+        private string buildNumber = "v15-";
         private Queue<Statistic> writeQueue = new Queue<Statistic>();
         private object lockObject = new object();
         private bool isWriting = false;
